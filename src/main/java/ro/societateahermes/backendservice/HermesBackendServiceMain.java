3794448858d2a871973e3c4b06e1373ea50e329c
Cojocaru Cosmin <cosmin.cojocaru@societatea-hermes.ro>
2021-07-14 10:43:30 +0300

    Files added

---
+++ src/main/java/ro/societateahermes/backendservice/HermesBackendServiceMain.java
@@ -0,0 +1,11 @@
+package ro.societateahermes.backendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesBackendServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(HermesBackendServiceMain.class, args);
+    }
+}
