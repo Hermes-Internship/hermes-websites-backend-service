a5c675dfd1ec7add34c3fc9fc5eadfff549cec24
mircea216 <panaitemircea378@yahoo.com>
2021-07-19 14:38:34 +0300

    del

--- src/main/java/ro/societateahermes/backendservice/controller/ImageControllerInterface.java
+++ src/main/java/ro/societateahermes/backendservice/controller/ImageControllerInterface.java
@@ -1,7 +1,6 @@
 package ro.societateahermes.backendservice.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
