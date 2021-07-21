9a4ee9d34125eb70a92d88c776c61c6ebec0e83e
Dragos Han <62242870+dragoshan20@users.noreply.github.com>
2021-07-21 10:25:48 +0300

    Revert "Event in progress switch"

--- src/main/java/ro/societateahermes/backendservice/repository/EventRepositoryInterface.java
+++ src/main/java/ro/societateahermes/backendservice/repository/EventRepositoryInterface.java
@@ -3,10 +3,6 @@ package ro.societateahermes.backendservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ro.societateahermes.backendservice.entities.Event;
 
-import java.util.Optional;
 
 public interface EventRepositoryInterface extends JpaRepository<Event, Long> {
-
-    Optional<Event> findByEventName(String eventName);
-
 }
--- src/main/java/ro/societateahermes/backendservice/service/EventServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package ro.societateahermes.backendservice.service;
-
-import ro.societateahermes.backendservice.service.dto.NotificationSwitchDTO;
-
-public interface EventServiceInterface {
-
-    NotificationSwitchDTO getEventStatusByEventName(String eventName);
-
-}
