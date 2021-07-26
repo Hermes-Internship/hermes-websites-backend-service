f5f376b7ec7835998f83582ce4b3104c5e344724
Gabriela-Gabor <gaborgabriela2001@yahoo.com>
2021-07-26 11:27:38 +0300

    singin+singup

---
+++ src/main/java/ro/societateahermes/backendservice/controller/controllerImplementation/AuthController.java
@@ -0,0 +1,146 @@
+package ro.societateahermes.backendservice.controller.controllerImplementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import ro.societateahermes.backendservice.entities.Admin;
+import ro.societateahermes.backendservice.entities.ERole;
+import ro.societateahermes.backendservice.entities.Role;
+import ro.societateahermes.backendservice.payload.request.LoginRequest;
+import ro.societateahermes.backendservice.payload.request.SignupRequest;
+import ro.societateahermes.backendservice.payload.response.JwtResponse;
+import ro.societateahermes.backendservice.payload.response.MessageResponse;
+import ro.societateahermes.backendservice.repository.AdminRepositoryInterface;
+import ro.societateahermes.backendservice.repository.RoleRepositoryInterface;
+import ro.societateahermes.backendservice.security.jwt.JwtUtils;
+import ro.societateahermes.backendservice.security.services.AdminDetailsImpl;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    AdminRepositoryInterface adminRepository;
+
+    @Autowired
+    RoleRepositoryInterface roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        AdminDetailsImpl adminDetails = (AdminDetailsImpl) authentication.getPrincipal();
+        List<String> roles = adminDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        JwtResponse jwtResponse = new JwtResponse();
+        jwtResponse.setId(adminDetails.getId());
+        jwtResponse.setEmail(adminDetails.getEmail());
+        jwtResponse.setUsername(adminDetails.getUsername());
+        jwtResponse.setRoles(roles);
+        jwtResponse.setToken(jwt);
+
+        return ResponseEntity.ok(jwtResponse);
+
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (adminRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (adminRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new admin's account
+        Admin admin = new Admin();
+        admin.setUsername(signUpRequest.getUsername());
+        admin.setEmail(signUpRequest.getEmail());
+        admin.setPassword(encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "cariereIT":
+                        Role cariereRole = roleRepository.findByName(ERole.ROLE_PROJECT_MANAGER_CARIEREIT)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(cariereRole);
+
+                        break;
+                    case "guideDays":
+                        Role guideRole = roleRepository.findByName(ERole.ROLE_PROJECT_MANAGER_GUIDEDAYS)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(guideRole);
+
+                        break;
+                    case "hackathon":
+                        Role hackathonRole = roleRepository.findByName(ERole.ROLE_PROJECT_MANAGER_HACKATHON)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(hackathonRole);
+
+                        break;
+                    case "destresiune":
+                        Role destresiuneRole = roleRepository.findByName(ERole.ROLE_PROJECT_MANAGER_DESTRESIUNE)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(destresiuneRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        admin.setRoles(roles);
+        adminRepository.save(admin);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+}
---
+++ src/main/java/ro/societateahermes/backendservice/security/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+package ro.societateahermes.backendservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ro.societateahermes.backendservice.security.jwt.AuthEntryPointJwt;
+import ro.societateahermes.backendservice.security.jwt.AuthTokenFilter;
+import ro.societateahermes.backendservice.security.services.AdminDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+		// securedEnabled = true,
+		// jsr250Enabled = true,
+		prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	AdminDetailsServiceImpl adminDetailsService;
+
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
+
+	@Bean
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(adminDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable()
+			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+			.anyRequest().authenticated();
+
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+}
