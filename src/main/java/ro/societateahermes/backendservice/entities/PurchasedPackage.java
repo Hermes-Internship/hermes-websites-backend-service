8fba15b345e22bbac925b71298c3ecf27cfbc76e
Mateioo7 <chismatei@gmail.com>
2021-07-30 09:36:12 +0300

    updated PurchasedPackage enum so it saves the string

--- src/main/java/ro/societateahermes/backendservice/entities/PurchasedPackage.java
+++ src/main/java/ro/societateahermes/backendservice/entities/PurchasedPackage.java
@@ -1,7 +1,7 @@
 package ro.societateahermes.backendservice.entities;
-public enum PurchasedPackage {
 
-    Silver,
-    Gold,
-    Bronze
+public enum PurchasedPackage {
+    BRONZE,
+    SILVER,
+    GOLD
 }
