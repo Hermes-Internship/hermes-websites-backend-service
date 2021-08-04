79ece0a64a635cb0545c1f72e1ca926e914713d7
Mateioo7 <chismatei@gmail.com>
2021-08-04 13:18:05 +0300

    renamed the cors filter as CorsFilter class already exists in Spring

--- src/main/java/ro/societateahermes/backendservice/configuration/SimpleCorsFilter.java
+++ src/main/java/ro/societateahermes/backendservice/configuration/SimpleCorsFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class CorsFilter implements Filter {
+public class SimpleCorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
